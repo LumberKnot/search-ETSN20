d9bd71853472328d5020f758724d7b08426699a3
Eric Weidow <85724887+Studsministern@users.noreply.github.com>
2024-11-28 10:42:51 +0100

    initial project structure

---
+++ src/main/Search.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Search {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/TestSearch.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+import main.Search;
+
+public class TestSearch {
+  @Test
+  public void testMain() throws Exception {
+    Search.main(new String[0]);
+  }
+}
